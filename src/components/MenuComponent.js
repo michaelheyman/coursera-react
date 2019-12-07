67b4738875860071688dfb96280b1640ac469070
michaelheyman <mitch.heyman@gmail.com>
2019-12-07 10:07:03 -0800

    A1: Extract dish detail component

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import {
-  Card, CardImg, CardImgOverlay, CardText, CardBody,
+  Card, CardImg, CardImgOverlay,
   CardTitle
 } from 'reactstrap';
+import DishDetail from "./DishdetailComponent"
 
 class Menu extends Component {
 
@@ -16,23 +17,6 @@ class Menu extends Component {
 
   onDishSelect(dish) {
     this.setState({ selectedDish: dish });
-  }
-
-  renderDish(dish) {
-    if (dish != null)
-      return (
-        <Card>
-          <CardImg top src={dish.image} alt={dish.name} />
-          <CardBody>
-            <CardTitle>{dish.name}</CardTitle>
-            <CardText>{dish.description}</CardText>
-          </CardBody>
-        </Card>
-      );
-    else
-      return (
-        <div></div>
-      );
   }
 
   render() {
@@ -55,11 +39,7 @@ class Menu extends Component {
         <div className="row">
           {menu}
         </div>
-        <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            {this.renderDish(this.state.selectedDish)}
-          </div>
-        </div>
+        <DishDetail dish={this.state.selectedDish}/>
       </div>
     );
   }
